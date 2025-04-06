6066e193f5902c7fd3ec25482098084972624c5a
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-06 02:07:23 -0300

    Refatoração

--- CalcularVolumeCaixa.ConsoleApp/Program.cs
+++ CalcularVolumeCaixa.ConsoleApp/Program.cs
@@ -16,17 +16,17 @@
 
             Console.WriteLine("Para começar, irei precisar das medidas de sua caixa:");
             Console.WriteLine("----------------------------------------");
-            Console.WriteLine("digite a medida do comprimento da caixa ");
+            Console.WriteLine("Digite a medida do comprimento da caixa ");
 
             decimal comprimento = Convert.ToDecimal(Console.ReadLine());
 
             Console.WriteLine("----------------------------------------");
-            Console.WriteLine("2 - digite a medida do largura da caixa");
+            Console.WriteLine("Digite a medida do largura da caixa");
 
             decimal largura = Convert.ToDecimal(Console.ReadLine());
 
             Console.WriteLine("----------------------------------------");
-            Console.WriteLine("2 - digite a medida do largura da caixa");
+            Console.WriteLine("Digite a medida do largura da caixa");
 
             decimal altura = Convert.ToDecimal(Console.ReadLine());
 
