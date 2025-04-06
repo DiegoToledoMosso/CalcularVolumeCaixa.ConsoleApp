27c3e8af446ff62eac7f101b5283226084dd6654
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-06 01:54:13 -0300

    Add project files.

---
+++ CalcularVolumeCaixa.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CalcularVolumeCaixa.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+
+            int resultado = 0;
+            
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Olá, vamos calcular o volume da sua caixa?");
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine("Para começar, irei precisar das medidas de sua caixa:");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("digite a medida do comprimento da caixa ");
+
+            int comprimento = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("2 - digite a medida do largura da caixa");
+
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("2 - digite a medida do largura da caixa");
+
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("----------------------------------------");
+
+
+            
+            resultado = comprimento * largura * altura;
+            
+
+            Console.WriteLine("Obrigado pelos dados,o volume da sua caixa é : " + resultado);
+
+        }
+    }
+}
